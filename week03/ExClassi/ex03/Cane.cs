52a26c5527ed3e8565daa8f25a11be506218d8ca
felipc273 <133594425+felipc247@users.noreply.github.com>
2023-12-13 16:19:35 +0100

    Add files via upload

---
+++ week03/ExClassi/ex03/Cane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Animali
+{
+    internal class Cane : Animale
+    {
+        public Cane(string nome) : base(nome)
+        {
+            this.Nome = nome;
+        }
+
+        public override void FaiVerso()
+        {
+            Console.WriteLine("BAUUUUUU");
+        }
+    }
+}
