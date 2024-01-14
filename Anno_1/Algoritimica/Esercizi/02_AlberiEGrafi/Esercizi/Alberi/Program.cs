c3838ffab793eec393c06f3c170ad492343142ce
felipc273 <133594425+felipc247@users.noreply.github.com>
2024-01-14 17:50:49 +0100

    Update Program.cs

--- Anno_1/Algoritimica/Esercizi/02_AlberiEGrafi/Esercizi/Alberi/Program.cs
+++ Anno_1/Algoritimica/Esercizi/02_AlberiEGrafi/Esercizi/Alberi/Program.cs
@@ -46,7 +46,49 @@ namespace Alberi_esempio
                 Radice = null;
             }
 
-            // stampa
+            // ricerca
+
+            public void Ricerca(int value) {
+                if (Ricerca_Rec(Radice, value))
+                {
+                    Console.WriteLine("Found");
+                }
+                else {
+                    Console.WriteLine("NOT Found");
+                }
+            }
+
+            private bool Ricerca_Rec(NodoAlberoBinario nodo, int value) {
+                bool found = false;
+                // Albero vuoto
+                if (nodo == null) { return found; }
+                // Valore trovato, return true
+                if (nodo.Valore == value) { found = true; return found; }
+                // Controllo se devo cercare nel ramo SX o ramo DX
+                if (value < nodo.Valore)
+                {
+                    // Se figlio SX esiste posso cercare tra i suoi figli
+                    if (nodo.Sinistro != null)
+                    {
+                        found = Ricerca_Rec(nodo.Sinistro, value);
+                        // Se trovato return true
+                        if (found) return found;
+                    }                   
+                }
+                else if (value > Radice.Valore)
+                {
+                    // Se figlio DX esiste posso cercare tra i suoi figli
+                    if (nodo.Destro != null)
+                    {
+                        found = Ricerca_Rec(nodo.Destro, value);
+                        // Se trovato return true
+                        if (found) return found;
+                    }
+                }
+                return found;
+            }
+
+            // stampa in-order
 
             public void Print_Tree()
             {
@@ -56,18 +98,19 @@ namespace Alberi_esempio
             private void Print_Tree_Rec(NodoAlberoBinario nodo, int livello)
             {
                 if (nodo == null) { Console.WriteLine("Nessun nodo"); return; }
+                if (nodo.Sinistro != null) Print_Tree_Rec(nodo.Sinistro, livello + 1);
                 Console.WriteLine($"livello => {livello}");
                 Console.WriteLine($"{nodo.Valore}");
-                if (nodo.Sinistro != null) Print_Tree_Rec(nodo.Sinistro, livello + 1);
                 if (nodo.Destro != null) Print_Tree_Rec(nodo.Destro, livello + 1);
             }
 
-            // Metodi di inserimento, ricerca e cancellazione possono essere implementati qui
+            // Inserimento valore
 
             public void Inserisci_Valore(int value)
             {
                 Inserisci_Valore_Rec(Radice, value);
             }
+
             private void Inserisci_Valore_Rec(NodoAlberoBinario nodo, int value)
             {
                 if (nodo == null) { Radice = new NodoAlberoBinario(value); return; }
@@ -99,6 +142,7 @@ namespace Alberi_esempio
         static void Main(string[] args)
         {
             AlberoBinarioDiRicerca abr = new AlberoBinarioDiRicerca();
+            // Popolamento abr
             abr.Inserisci_Valore(50);
             abr.Inserisci_Valore(51);
             abr.Inserisci_Valore(53);
@@ -110,7 +154,10 @@ namespace Alberi_esempio
             abr.Inserisci_Valore(564);
             abr.Inserisci_Valore(-1);
             abr.Inserisci_Valore(0);
+            // Stampa
             abr.Print_Tree();
+            // Ricerca
+            abr.Ricerca(564);
         }
     }
 }
