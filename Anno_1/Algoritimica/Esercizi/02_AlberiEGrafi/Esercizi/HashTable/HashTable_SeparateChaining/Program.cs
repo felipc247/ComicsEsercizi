956375c39ccc1dd69cfee778a6620246e43cdd07
felipc273 <133594425+felipc247@users.noreply.github.com>
2024-04-29 15:44:46 +0200

    Create Program.cs

---
+++ Anno_1/Algoritimica/Esercizi/02_AlberiEGrafi/Esercizi/HashTable/HashTable_SeparateChaining/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable_Separate_chaining
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HashTable<int, string> hashTable = new HashTable<int, string>(3);
+            int v1 = 3, v2 = 4, v3 = 5, v4 = 6;
+            hashTable.Add_value(v1, "coto", v1.GetHashCode());
+            hashTable.Add_value(v2, "re", v2.GetHashCode());
+            hashTable.Add_value(v3, "wd", v3.GetHashCode());
+            hashTable.Add_value(v4, "bd", v4.GetHashCode());
+            hashTable.Add_value(v4, "bd", v4.GetHashCode());
+            try
+            {
+                hashTable.Delete_value(v2, "re");
+                Console.WriteLine("Coppia Chiave/valore trovati ed eliminati");
+                hashTable.Find_value(v2, "re");
+                Console.WriteLine("Coppia Chiave/valore trovati");
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Coppia Chiave/valore NON trovati");
+            }
+        }
+    }
+}
